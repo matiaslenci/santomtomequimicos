6552c494244c65cff81d4ba438b4f43bc748e874
Matias Lenci <matiaslenci@gmail.com>
2022-07-27 00:35:04 -0300

    primer commit

---
+++ js/index.js
@@ -0,0 +1,39 @@
+/* --------OBJETOS-------- */
+
+const productos = [
+    {
+      id: 0,
+      nombre: "Jabon liquido",
+      precio: 1000,
+      img: "'../img/muestra.jpg'",
+    },
+    {
+      id: 1,
+      nombre: "Jabon liquido",
+      precio: 1000,
+      img: "'../img/muestra.jpg'",
+    },
+    {
+      id: 2,
+      nombre: "Jabon liquido",
+      precio: 1000,
+      img: "'../img/muestra.jpg'",
+    },
+    {
+      id: 3,
+      nombre: "Jabon liquido",
+      precio: 1000,
+      img: "'../img/muestra.jpg'",
+    },
+    {
+      id: 4,
+      nombre: "Jabon liquido",
+      precio: 1000,
+      img: "'../img/muestra.jpg'",
+    },
+];
+
+let lista = document.getElementById('lista');
+
+lista.innerHTML = `
+`
