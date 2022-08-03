0f15aff07d84e2e176c7bd7e08009be7451de806
Matias Lenci <matiaslenci@gmail.com>
2022-08-03 10:39:04 -0300

    productos agregados

--- js/index.js
+++ js/index.js
@@ -6,35 +6,59 @@ const productos = [
       nombre: "Jabon liquido",
       precio: 1000,
       img: "'../img/muestra.jpg'",
+      descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae repellat itaque, ducimus est neque expedita non saepe, eum repudiandae aperiam vero. Sunt enim quos, harum sint excepturi odio reprehenderit in."
     },
     {
       id: 1,
       nombre: "Jabon liquido",
       precio: 1000,
       img: "'../img/muestra.jpg'",
+      descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae repellat itaque, ducimus est neque expedita non saepe, eum repudiandae aperiam vero. Sunt enim quos, harum sint excepturi odio reprehenderit in."
     },
     {
       id: 2,
       nombre: "Jabon liquido",
       precio: 1000,
       img: "'../img/muestra.jpg'",
+      descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae repellat itaque, ducimus est neque expedita non saepe, eum repudiandae aperiam vero. Sunt enim quos, harum sint excepturi odio reprehenderit in."
     },
     {
       id: 3,
       nombre: "Jabon liquido",
       precio: 1000,
       img: "'../img/muestra.jpg'",
+      descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae repellat itaque, ducimus est neque expedita non saepe, eum repudiandae aperiam vero. Sunt enim quos, harum sint excepturi odio reprehenderit in."
     },
     {
       id: 4,
       nombre: "Jabon liquido",
       precio: 1000,
       img: "'../img/muestra.jpg'",
+      descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae repellat itaque, ducimus est neque expedita non saepe, eum repudiandae aperiam vero. Sunt enim quos, harum sint excepturi odio reprehenderit in."
     },
 ];
 
-let lista = document.getElementById('lista');
-/* 
-lista.innerHTML = `
 
-` */
+function renderizarProductos(){
+let lista = document.getElementById('lista');
+for (const producto of productos){ 
+lista.innerHTML += ` 
+<li class="list-group-item li--producto">
+<div class="card">
+  <div class="img--contenedor">
+    <img src=${producto.img} class="img-fluid">
+  </div>
+  <div class="info--contenedor">
+    <h3>${producto.nombre}</h3>
+    <p><strong> $ ${producto.precio} </strong></p>
+    <p>${producto.descripcion}</p>
+    <button class='btn btn--producto' id='btn${producto.id}'>Comprar</button>
+  </div>
+</div>
+</li>
+`;
+
+}
+ }
+
+  renderizarProductos();
